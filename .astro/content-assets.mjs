78693ed8596ba87be656fab8b406ed9b25b6f177
kamaldhingra1 <dhingra_kk@yahoo.com>
2025-09-23 21:58:05 -0400

    Fix

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,5 @@
 
+import __ASTRO_IMAGE_IMPORT_Z1cFbA6 from "./spongebob.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F01-Getting-Started%2Findex.md";
 import __ASTRO_IMAGE_IMPORT_Z1sJARh from "./spongebob.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F04-astro-sphere-writing-markdown%2Findex.md";
-export default new Map([["./spongebob.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F04-astro-sphere-writing-markdown%2Findex.md", __ASTRO_IMAGE_IMPORT_Z1sJARh]]);
+export default new Map([["./spongebob.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F01-Getting-Started%2Findex.md", __ASTRO_IMAGE_IMPORT_Z1cFbA6], ["./spongebob.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F04-astro-sphere-writing-markdown%2Findex.md", __ASTRO_IMAGE_IMPORT_Z1sJARh]]);
 		
