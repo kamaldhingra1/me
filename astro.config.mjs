b600252f0717552c11089f53f3a67cdf379c9af2
kamaldhingra1 <dhingra_kk@yahoo.com>
2025-10-04 23:00:40 -0400

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import solidJs from "@astrojs/solid-js"
 // https://astro.build/config
 export default defineConfig({
   site: "https://kamaldhingra.com",
-  base: "",
+  base: "/",
   outDir: './docs',
   output: "static",
   build: {
